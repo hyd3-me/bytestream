3e1028d5c6440acf8d6a01c1edda8af46d5865cd
sedg <hyde.rou@yandex.ru>
2026-03-12 17:12:24 +0300

    test(room): add test for get_dm_room_id (red)

--- backend/tests/unit/test_room_utils.py
+++ backend/tests/unit/test_room_utils.py
@@ -8,3 +8,10 @@ def test_sort_addresses_returns_sorted_pair():
     sorted1, sorted2 = utils.sort_addresses(addr1, addr2)
     assert sorted1 == "0xaaa"
     assert sorted2 == "0xbbb"
+
+
+def test_get_dm_room_id_returns_deterministic_id():
+    addr_a = "0xbbb"
+    addr_b = "0xaaa"
+    room_id = utils.get_dm_room_id(addr_a, addr_b)
+    assert room_id == "dm:0xaaa:0xbbb"
