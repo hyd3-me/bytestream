46109baca92c7955182b90f4e988a7bc37e1b50b
sedg <hyde.rou@yandex.ru>
2026-03-24 18:52:34 +0300

    test(room): add test for existing room case (red)

--- backend/tests/integration/test_room_invitation.py
+++ backend/tests/integration/test_room_invitation.py
@@ -1,5 +1,6 @@
 import pytest
 from app.ws import manager
+from app.room import utils
 
 
 @pytest.mark.asyncio
@@ -85,3 +86,28 @@ async def test_handle_create_room_request_returns_early_if_target_address_missin
     mock_get_session.assert_awaited_once_with(sid)
     mock_is_valid.assert_not_called()
     mock_emit.assert_not_called()
+
+
+@pytest.mark.asyncio
+async def test_handle_create_room_request_joins_existing_room(mocker):
+    sid = "test_sid"
+    address_a = "0xaaa"
+    address_b = "0xbbb"
+    data = {"target_address": address_b}
+    room_id = utils.get_dm_room_id(address_a, address_b)
+
+    mock_get_session = mocker.patch.object(
+        manager.ws_manager.sio, "get_session", return_value={"address": address_a}
+    )
+    mock_room_exists = mocker.patch("app.room.crud.room_exists", return_value=True)
+    mock_enter_room = mocker.patch.object(manager.ws_manager.sio, "enter_room")
+    mock_emit = mocker.patch.object(manager.ws_manager.sio, "emit")
+    mock_is_valid = mocker.patch(
+        "app.room.utils.is_valid_eth_address", return_value=True
+    )
+
+    await manager.ws_manager.handle_create_room_request(sid, data)
+
+    mock_room_exists.assert_awaited_once()
+    mock_enter_room.assert_awaited_once_with(sid, room_id)
+    mock_emit.assert_awaited_once_with("room_ready", {"room_id": room_id}, room=sid)
