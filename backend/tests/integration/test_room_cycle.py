71f18d04ea0583659775f729f271a1dfee07f73a
sedg <hyde.rou@yandex.ru>
2026-08-28 14:59:07 +0300

    test: add failing integration test for socketio connect (RED)

---
+++ backend/tests/integration/test_room_cycle.py
@@ -0,0 +1,13 @@
+import pytest
+import socketio
+
+
+@pytest.mark.asyncio
+async def test_socketio_connect_with_jwt(room_users, socketio_client_factory):
+    user_a = room_users["a"]
+
+    sio = await socketio_client_factory(user_a["token"])
+
+    assert sio.connected is True
+
+    await sio.disconnect()
